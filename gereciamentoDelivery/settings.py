ee5682277828b3434d02371986aac03518fbfdd9
Willyam Cutrim <37386568+willcutrim@users.noreply.github.com>
2024-01-06 23:59:32 -0300

    Update settings.py

--- gereciamentoDelivery/settings.py
+++ gereciamentoDelivery/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = 'gereciamentoDelivery.wsgi.app'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
 
