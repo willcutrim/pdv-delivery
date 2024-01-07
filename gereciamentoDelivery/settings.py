c27032e9a4aef3df23e2a2f06e0fefa52b2451fe
Willyam Cutrim <37386568+willcutrim@users.noreply.github.com>
2024-01-07 00:01:08 -0300

    Update settings.py

--- gereciamentoDelivery/settings.py
+++ gereciamentoDelivery/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'gereciamentoDelivery.wsgi.app'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join('/tmp', 'db.sqlite3'),
+    # }
 }
 
 
